8bbb36d74c151089b47d8a69bddc3543451a6d04
Justin Jeffries <justincyjeffries@gmail.com>
2022-11-22 14:37:21 -0500

    commit tree b1a168ceab98b6f8d019df9c0149607f471cbefc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, uniqueness: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    
+    validate :clickbait
+
+    def clickbait
+        if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top" || "Guess")
+            errors.add(:title, "Be a clickbaiter")
+        end
+    end
 end
